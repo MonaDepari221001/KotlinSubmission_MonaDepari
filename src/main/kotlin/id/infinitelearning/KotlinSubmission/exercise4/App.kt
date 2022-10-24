aca1212885d36e5217e821e69786e1d628c726b0
MONALISA <monalisadepari@gmail.com>
2022-10-24 17:09:41 +0700

    first commit

---
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
@@ -0,0 +1,28 @@
+package id.infinitelearning.KotlinSubmission.exercise4
+
+fun main() {
+
+    /**
+     * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
+     *
+     */
+    try {
+        validation("Monalisa depari")
+        validation("")
+    }
+    catch (error : Exception){
+        println( "${error.message}")
+
+    }
+}
+
+fun validation (name:String)  {
+    if (name.isBlank()){
+        throw Exception ("Nama tidak boleh kosong") }
+    else {
+        println("Hello $name")
+    }
+
+
+
+}
